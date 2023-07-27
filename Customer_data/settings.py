dc2aabbf1a9fb707a45ccd0fb9ebef63a5465cb3
tanmayysain <tanmaysain02@gmail.com>
2023-07-27 21:55:54 +0530

    Done vercel Setup

--- Customer_data/settings.py
+++ Customer_data/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)wh3^draahg(j-nfu3t9&-ily21(bo6pl27$sznume@7(=l@(n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
