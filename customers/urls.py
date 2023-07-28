74b2ad951eab94c2e6d364619bcff2d749589680
tanmayysain <tanmaysain02@gmail.com>
2023-07-28 23:49:25 +0530

    static

--- customers/urls.py
+++ customers/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,5 +12,7 @@ urlpatterns = [
     path('delete/<int:pk>', views.delete_record, name='delete_record'), 
     path('add_record/', views.add_record, name='add_record'),
     path('update/<int:pk>', views.update_record, name='update_record'), 
-   
 ]
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
